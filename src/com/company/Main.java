a2f5f34c288a2ceb3dfca8edfd6ee14e99ce82d5
evboggs302 <evboggs94@gmail.com>
2019-10-10 11:59:51 -0400

    solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 //        Create a method called getDurationString with two parameters, first parameter minutes and 2nd parameter seconds.
 //        You should validate that the first parameter minutes is >= 0.
 //        You should validate that the 2nd parameter seconds is >= 0 and <= 59.
-//        The method should return ÅgInvalid valueÅh in the method if either of the above are not true.
-//        If the parameters are valid then calculate how many hours minutes and seconds equal the minutes and seconds passed to this method and return that value as string in format ÅgXXh YYm ZZsÅh where XX represents a number of hours, YY the minutes and ZZ the seconds.
+//        The method should return "Invalid value" in the method if either of the above are not true.
+//        If the parameters are valid then calculate how many hours minutes and seconds equal the minutes and seconds passed to this method and return that value as string in format "XXh YYm ZZs" where XX represents a number of hours, YY the minutes and ZZ the seconds.
 //        Create a 2nd method of the same name but with only one parameter seconds.
-//        Validate that it is >= 0, and return ÅgInvalid valueÅh if it is not true.
+//        Validate that it is >= 0, and return "Invalid value" if it is not true.
 //        If it is valid, then calculate how many minutes are in the seconds value and then call the other overloaded method passing the correct minutes and seconds calculated so that it can calculate correctly.
 //        Call both methods to print values to the console.
 //
@@ -23,6 +23,31 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945L));
 
+    }
+
+    private static String getDurationString(long minutes, long seconds){
+        if((minutes < 0) || (seconds < 0) || (seconds > 59)){
+            return "Invalid Value";
+        }
+
+        long hours = minutes / 60;
+        long remainingMins = minutes % 60;
+
+        return hours + "h " + remainingMins + "m " + seconds + "s";
+    }
+
+    private static String getDurationString(long seconds){
+        if(seconds < 0 ){
+            return "Invalid Value";
+        }
+
+        long minutes = seconds / 60;
+        long remainingSecs = seconds % 60;
+//        String answer = getDurationString(minutes, remainingSecs);
+//        return answer;
+        return getDurationString(minutes, remainingSecs);
     }
 }
